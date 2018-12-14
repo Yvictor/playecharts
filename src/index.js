bc9ad4fd3811f8db8217e2498bdd11c4cb18c62d
yvictor <victor.yang@sinopac.com>
2018-12-14 16:11:18 +0800

    tutorial pie

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import echarts from 'echarts'
 window.onload = function () {
     // 基于准备好的dom，初始化echarts实例
     let myChart = echarts.init(document.getElementById('main'));
-    // 绘制图表
-    myChart.setOption({
+    let optionBarChart = {
         title: {
             text: 'ECharts playing'
         },
@@ -18,6 +17,59 @@ window.onload = function () {
             type: 'bar',
             data: [5, 20, 36, 10, 10, 20]
         }]
-    });
+    }
+
+    let optionPieChart = {
+        backgroundColor: '#2c343c',
+        itemStyle: {
+            emphasis: {
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+            }
+        },
+        textStyle: {
+            color: 'rgba(255, 255, 255, 0.3)'
+        },
+        labelLine: {
+            lineStyle: {
+                color: 'rgba(255, 255, 255, 0.3)'
+            }
+        },
+        itemStyle: {
+            // 设置扇形的颜色
+            color: '#c23531',
+            shadowBlur: 200,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+        },
+        visualMap: {
+            // 不显示 visualMap 组件，只用于明暗度的映射
+            show: false,
+            // 映射的最小值为 80
+            min: 80,
+            // 映射的最大值为 600
+            max: 600,
+            inRange: {
+                // 明暗度的范围是 0 到 1
+                colorLightness: [0, 1]
+            }
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                roseType: 'angle',
+                radius: '55%',
+                data: [
+                    { value: 235, name: '视频广告' },
+                    { value: 274, name: '联盟广告' },
+                    { value: 310, name: '邮件营销' },
+                    { value: 335, name: '直接访问' },
+                    { value: 400, name: '搜索引擎' }
+                ]
+            }
+        ]
+    }
+    // 绘制图表
+    myChart.setOption(optionPieChart);
 }
 
